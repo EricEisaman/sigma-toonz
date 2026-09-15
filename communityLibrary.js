bd35f979901cadc2f1db53ad1615c8a0576e3db6
SirFizX <eeisaman@auburnschl.edu>
2026-09-15 06:06:58 -0400

    SIGGIN STYLE

--- communityLibrary.js
+++ communityLibrary.js
@@ -5,6 +5,12 @@ class CommunityLibrary {
     // Initial community folders - hard coded for now
     this.communityFolders = [
       {
+        id: '180HT1V0-HWwKuug_dMsXIrlQkKEcqDzv',
+        name: 'SIGGIN STYLE',
+        description: 'A collection of cool rap toonz.',
+        tags: ['flex', 'math', 'tech', 'community', 'sigma scholars']
+      },
+      {
         id: '15tSXyTvxzTkNFmdC6EOG4Z27c2oN7ZFC',
         name: 'ACADEMIC BIGGIE!',
         description: 'A collection of cool rap toonz.',
